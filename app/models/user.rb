2ad47666114609e21886f76c16fbc94967ef9992
Dennis Kamau <devjam.ke@gmail.com>
2024-09-13 15:53:14 +0300

    generate the Airlines Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { customer: 0, manager: 1, admin: 2 }
 end
