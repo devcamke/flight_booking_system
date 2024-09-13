4210dcb8482a176c6b5fc0fbf327aa9156a91c8c
Dennis Kamau <devjam.ke@gmail.com>
2024-09-13 16:54:59 +0300

    Redesign the Flight Views

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,15 @@
 class FlightsController < ApplicationController
   def index
+    @flights = Flight.all
   end
 
   def show
+    @flights = Flight.find(params[:id])
   end
 
   def search
+    @flights = Flight.where(origin: params[:origin], destination: params[:destination])
+    .where('departure >= ?', params[:departure_date])
+    render :index
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_124859) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_13_125635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airlines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "flight_id", null: false
+    t.bigint "seat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["seat_id"], name: "index_bookings_on_seat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.string "number"
+    t.datetime "departure"
+    t.datetime "arrival"
+    t.string "origin"
+    t.string "destination"
+    t.bigint "airline_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
+  end
+
+  create_table "seat_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "seats", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.bigint "seat_type_id", null: false
+    t.decimal "price"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_seats_on_flight_id"
+    t.index ["seat_type_id"], name: "index_seats_on_seat_type_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +72,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_124859) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "seats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flights", "airlines"
+  add_foreign_key "seats", "flights"
+  add_foreign_key "seats", "seat_types"
 end
